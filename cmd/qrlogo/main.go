cb35e53b3fde729c93e4c042143ab65c584e8c70
Ivan Daniluk <ivan.daniluk@gmail.com>
2016-05-18 18:11:27 +0200

    Initial commit

---
+++ cmd/qrlogo/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/divan/qrlogo"
+	"image"
+	_ "image/png"
+	"os"
+)
+
+var (
+	input  = flag.String("i", "logo.png", "Logo to be placed over QR code")
+	output = flag.String("o", "qr.png", "Output filename")
+	size   = flag.Int("size", 512, "Image size in pixels")
+)
+
+func main() {
+	flag.Usage = Usage
+	flag.Parse()
+
+	if flag.NArg() != 1 {
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	text := flag.Arg(0)
+
+	file, err := os.Open(*input)
+	errcheck(err, "Failed to open logo:")
+	defer file.Close()
+
+	logo, _, err := image.Decode(file)
+	errcheck(err, "Failed to decode PNG with logo:")
+
+	qr, err := qrlogo.Encode(text, logo, *size)
+	errcheck(err, "Failed to encode QR:")
+
+	out, err := os.Create(*output)
+	errcheck(err, "Failed to open output file:")
+	out.Write(qr.Bytes())
+	out.Close()
+
+	fmt.Println("Done! Written QR image to", *output)
+}
+
+// Usage overloads flag.Usage.
+func Usage() {
+	fmt.Fprintln(os.Stderr, "Usage: qrlogo [options] text")
+	flag.PrintDefaults()
+}
+
+func errcheck(err error, str string) {
+	if err != nil {
+		fmt.Println(str, err)
+		os.Exit(1)
+	}
+}
---
+++ qrlogo.go
@@ -0,0 +1,67 @@
+package qrlogo
+
+import (
+	"bytes"
+	qr "github.com/skip2/go-qrcode"
+	"image"
+	"image/color"
+	"image/png"
+)
+
+// Encoder defines settings for QR/Overlay encoder.
+type Encoder struct {
+	AlphaThreshold int
+	GreyThreshold  int
+	QRLevel        qr.RecoveryLevel
+}
+
+// DefaultEncoder is the encoder with default settings.
+var DefaultEncoder = Encoder{
+	AlphaThreshold: 2000,       // FIXME: don't remember where this came from
+	GreyThreshold:  30,         // in percent
+	QRLevel:        qr.Highest, // recommended, as logo steals some redundant space
+}
+
+// Encode encodes QR image, adds logo overlay and renders result as PNG.
+func Encode(str string, logo image.Image, size int) (*bytes.Buffer, error) {
+	return DefaultEncoder.Encode(str, logo, size)
+}
+
+// Encode encodes QR image, adds logo overlay and renders result as PNG.
+func (e Encoder) Encode(str string, logo image.Image, size int) (*bytes.Buffer, error) {
+	var buf bytes.Buffer
+
+	code, err := qr.New(str, e.QRLevel)
+	if err != nil {
+		return nil, err
+	}
+
+	img := code.Image(size)
+	e.overlayLogo(img, logo)
+
+	err = png.Encode(&buf, img)
+	if err != nil {
+		return nil, err
+	}
+
+	return &buf, nil
+}
+
+// overlayLogo blends logo to the center of the QR code,
+// changing all colors to black.
+func (e Encoder) overlayLogo(dst, src image.Image) {
+	grey := uint32(^uint16(0)) * uint32(e.GreyThreshold) / 100
+	alphaOffset := uint32(e.AlphaThreshold)
+	offset := dst.Bounds().Max.X/2 - src.Bounds().Max.X/2
+	for x := 0; x < src.Bounds().Max.X; x++ {
+		for y := 0; y < src.Bounds().Max.Y; y++ {
+			if r, g, b, alpha := src.At(x, y).RGBA(); alpha > alphaOffset {
+				col := color.Black
+				if r > grey && g > grey && b > grey {
+					col = color.White
+				}
+				dst.(*image.RGBA).Set(x+offset, y+offset, col)
+			}
+		}
+	}
+}

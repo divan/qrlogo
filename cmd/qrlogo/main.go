bc8ac1f35bb98ef659ad16535108b7f44009c223
yaoguais <liuyong@juwang.cn>
2017-12-29 12:25:43 +0800

    add qrcode with bg

--- cmd/qrlogo/main.go
+++ cmd/qrlogo/main.go
@@ -7,14 +7,18 @@ import (
 	_ "image/png"
 	"os"
 
-	"github.com/divan/qrlogo"
+	"github.com/yaoguais/qrlogo"
 )
 
 var (
-	input  = flag.String("i", "logo.png", "Logo to be placed over QR code")
+	input  = flag.String("i", "", "Logo to be placed over QR code")
 	output = flag.String("o", "qr.png", "Output filename")
 	keep   = flag.Bool("k", true, "keep the color of logo")
 	size   = flag.Int("size", 512, "Image size in pixels")
+
+	bg      = flag.String("b", "", "Bg image for qrcode image")
+	offsetX = flag.Int("ox", 10, "Qrcode image offset X on bg image")
+	offsetY = flag.Int("oy", 20, "Qrcode image offset Y on bg image")
 )
 
 func main() {
@@ -28,6 +32,16 @@ func main() {
 
 	text := flag.Arg(0)
 
+	if *input != "" {
+		qrCodeWithlogo(text)
+	} else {
+		qrCodeWithBg(text)
+	}
+
+	fmt.Println("Done! Written QR image to", *output)
+}
+
+func qrCodeWithlogo(text string) {
 	file, err := os.Open(*input)
 	errcheck(err, "Failed to open logo:")
 	defer file.Close()
@@ -44,8 +58,23 @@ func main() {
 	errcheck(err, "Failed to open output file:")
 	out.Write(qr.Bytes())
 	out.Close()
+}
 
-	fmt.Println("Done! Written QR image to", *output)
+func qrCodeWithBg(text string) {
+	file, err := os.Open(*bg)
+	errcheck(err, "Failed to open bg image:")
+	defer file.Close()
+
+	bg, _, err := image.Decode(file)
+	errcheck(err, "Failed to decode PNG with bg image:")
+
+	qr, err := qrlogo.EncodeToBg(text, bg, *size, *offsetX, *offsetY, 0, 0, 0, 0)
+	errcheck(err, "Failed to encode QR:")
+
+	out, err := os.Create(*output)
+	errcheck(err, "Failed to open output file:")
+	out.Write(qr.Bytes())
+	out.Close()
 }
 
 // Usage overloads flag.Usage.
--- qrlogo.go
+++ qrlogo.go
@@ -30,6 +30,38 @@ func Encode(str string, logo image.Image, size int) (*bytes.Buffer, error) {
 	return DefaultEncoder.Encode(str, logo, size)
 }
 
+// EncodeToBg encodes QR image, adds to bg image overlay and renders result as PNG.
+func EncodeToBg(str string, bg image.Image, size, offsetX, offsetY, sx, sy, swidth, sheight int) (*bytes.Buffer, error) {
+	return DefaultEncoder.EncodeToBg(str, bg, size, offsetX, offsetY, sx, sy, swidth, sheight)
+}
+
+// EncodeToBg encodes QR image, adds to bg image overlay and renders result as PNG.
+func (e Encoder) EncodeToBg(str string, bg image.Image, size, offsetX, offsetY, sx, sy, swidth, sheight int) (*bytes.Buffer, error) {
+	var buf bytes.Buffer
+
+	code, err := qr.New(str, e.QRLevel)
+	if err != nil {
+		return nil, err
+	}
+
+	img := code.Image(size)
+	if swidth <= 0 {
+		swidth = img.Bounds().Max.X
+	}
+	if sheight <= 0 {
+		sheight = img.Bounds().Max.Y
+	}
+
+	e.overlayWithOffset(bg, img, sx, sy, swidth, sheight, offsetX, offsetY)
+
+	err = png.Encode(&buf, bg)
+	if err != nil {
+		return nil, err
+	}
+
+	return &buf, nil
+}
+
 // Encode encodes QR image, adds logo overlay and renders result as PNG.
 func (e Encoder) Encode(str string, logo image.Image, size int) (*bytes.Buffer, error) {
 	var buf bytes.Buffer
@@ -40,15 +72,11 @@ func (e Encoder) Encode(str string, logo image.Image, size int) (*bytes.Buffer, 
 	}
 
 	img := code.Image(size)
+	//e.overlayLogo(img, logo)
+	src := PalettedToRGBA(img.(*image.Paletted))
+	e.overlayOrinalLogo(src, logo)
 
-	if e.KeepColor {
-		img = PalettedToRGBA(img.(*image.Paletted))
-		e.overlayOriginalLogo(img, logo)
-	} else {
-		e.overlayLogo(img, logo)
-	}
-
-	err = png.Encode(&buf, img)
+	err = png.Encode(&buf, src)
 	if err != nil {
 		return nil, err
 	}
@@ -75,9 +103,9 @@ func (e Encoder) overlayLogo(dst, src image.Image) {
 	}
 }
 
-// overlayOriginalLogo blends logo to the center of the QR code,
+// overlayLogo blends logo to the center of the QR code,
 // keep colors.
-func (e Encoder) overlayOriginalLogo(dst, src image.Image) {
+func (e Encoder) overlayOrinalLogo(dst, src image.Image) {
 	offset := dst.Bounds().Max.X/2 - src.Bounds().Max.X/2
 	for x := 0; x < src.Bounds().Max.X; x++ {
 		for y := 0; y < src.Bounds().Max.Y; y++ {
@@ -86,7 +114,26 @@ func (e Encoder) overlayOriginalLogo(dst, src image.Image) {
 	}
 }
 
-// PalettedToRGBA convert platted to rgba
+func (e Encoder) overlayWithOffset(dst, src image.Image, sx, sy, swidth, sheight, offsetX, offsetY int) {
+	switch dst.(type) {
+	case *image.NRGBA:
+		dstRGBA := dst.(*image.NRGBA)
+		for x := 0; x < swidth; x++ {
+			for y := 0; y < sheight; y++ {
+				dstRGBA.Set(x+offsetX, y+offsetY, src.At(sx+x, sy+y))
+			}
+		}
+	case *image.RGBA:
+		dstRGBA := dst.(*image.RGBA)
+		for x := 0; x < swidth; x++ {
+			for y := 0; y < sheight; y++ {
+				dstRGBA.Set(x+offsetX, y+offsetY, src.At(sx+x, sy+y))
+			}
+		}
+	}
+}
+
+// PalettedToRGBA use unsafe.Pointer for optimize
 func PalettedToRGBA(src *image.Paletted) *image.RGBA {
 	dst := image.NewRGBA(src.Bounds())
 	for x := 0; x < src.Bounds().Max.X; x++ {
